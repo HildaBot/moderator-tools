66142c4f699bd53984754b7d13083d4801ea9034
jamietech <jamie@jamiete.ch>
2017-12-27 22:21:58 +1000

    Handle messages that are too old
    * This did not work with the old code.
    + Guess the time by allowing a 5 minute leeway for potentially good
    messages, erring on the side of them being too old.
    + Iterate all messages from server more effectively by using a
    non-caching method of retrieval.

--- src/ch/jamiete/hilda/moderatortools/runnables/ChannelClearTask.java
+++ src/ch/jamiete/hilda/moderatortools/runnables/ChannelClearTask.java
@@ -15,15 +15,18 @@
  *******************************************************************************/
 package ch.jamiete.hilda.moderatortools.runnables;
 
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import ch.jamiete.hilda.Hilda;
+import ch.jamiete.hilda.Start;
 import ch.jamiete.hilda.runnables.MessageDeletionTask;
 import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.TextChannel;
 import net.dv8tion.jda.core.entities.User;
-import net.dv8tion.jda.core.exceptions.ErrorResponseException;
+import net.dv8tion.jda.core.requests.restaction.pagination.MessagePaginationAction;
 
 public class ChannelClearTask implements Runnable {
     private final Hilda hilda;
@@ -48,6 +51,9 @@ public class ChannelClearTask implements Runnable {
 
         this.channel.sendTyping().queue();
 
+        final MessagePaginationAction action = this.channel.getIterableHistory().cache(false);
+        boolean old = false;
+
         while (this.amount > 0) {
             int run = this.user == null ? this.amount : 100;
 
@@ -57,40 +63,45 @@ public class ChannelClearTask implements Runnable {
 
             Hilda.getLogger().fine("Deleting " + run + " messages this run...");
 
-            final List<Message> messages = this.channel.getHistory().retrievePast(run).complete();
+            List<Message> messages = action.complete().stream().limit(run).collect(Collectors.toList());
+
+            if (Start.DEBUG)
+                Hilda.getLogger().fine("Received list of " + messages.size() + " messages.");
+
+            Instant too_old = Instant.now().minus(14, ChronoUnit.DAYS).minus(5, ChronoUnit.MINUTES);
+            if (messages.stream().anyMatch(m -> m.getCreationTime().toInstant().isBefore(too_old))) {
+                Hilda.getLogger().fine("Messages too old!");
+                old = true;
+                messages = messages.stream().filter(m -> m.getCreationTime().toInstant().isAfter(too_old)).collect(Collectors.toList());
+            }
 
             if (messages.isEmpty()) {
+                Hilda.getLogger().fine("Messages already empty.");
                 break;
             }
 
             Hilda.getLogger().fine("Deleting...");
 
-            try {
-                if (this.user != null) {
-                    List<Message> relevant = messages.stream().filter(message -> message.getAuthor() == this.user).collect(Collectors.toList());
+            if (this.user != null) {
+                List<Message> relevant = messages.stream().filter(message -> message.getAuthor() == this.user).collect(Collectors.toList());
 
-                    if (relevant.size() > this.amount) {
-                        relevant = relevant.subList(0, this.amount - 1);
-                    }
-
-                    this.channel.deleteMessages(relevant).complete();
-                    this.amount -= relevant.size();
-
-                    if (relevant.size() == 0) {
-                        Hilda.getLogger().fine("Gave up deleting user's messages; there's " + this.amount + " left");
-                        break;
-                    }
-                } else {
-                    this.channel.deleteMessages(messages).complete();
-                    this.amount -= run;
+                if (relevant.size() > this.amount) {
+                    relevant = relevant.subList(0, this.amount - 1);
                 }
-            } catch (final ErrorResponseException e) {
-                this.channel.sendMessage("I couldn't clear the messages in this channel as they're more than two weeks old.").queue();
-                Hilda.getLogger().info("Given up deleting because channel messages are too old.");
-                return;
+
+                this.channel.deleteMessages(relevant).complete();
+                this.amount -= relevant.size();
+
+                if (relevant.size() == 0) {
+                    Hilda.getLogger().fine("Gave up deleting user's messages; there's " + this.amount + " left");
+                    break;
+                }
+            } else {
+                this.channel.deleteMessages(messages).complete();
+                this.amount -= run;
             }
 
-            if (messages.size() < 100) { // Channel now empty
+            if (messages.size() < 100 || old) { // Channel now empty
                 break;
             }
 
@@ -99,8 +110,18 @@ public class ChannelClearTask implements Runnable {
 
         Hilda.getLogger().fine("Finished deleting!");
 
-        this.channel.sendMessage("Deleted messages.").queue(sent -> {
-            this.hilda.getExecutor().schedule(new MessageDeletionTask(sent), 5, TimeUnit.SECONDS);
+        String temp = "Deleted messages.";
+
+        if (old) {
+            temp += " Some messages were more than two weeks old and I can't delete any messages older than that. I've deleted everything I can, but I'm otherwise stopping deletions now.";
+        }
+
+        String response = temp;
+
+        this.channel.sendMessage(response).queue(sent -> {
+            if (response.length() > 32) {
+                this.hilda.getExecutor().schedule(new MessageDeletionTask(sent), 5, TimeUnit.SECONDS);
+            }
         });
     }
 
